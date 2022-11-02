fe3b13c907a69afe7cf0721bae5c29ddc16b23e2
abilbotz1 <108676663+abilbotz1@users.noreply.github.com>
2022-11-03 03:28:06 +0800

    Update info-donasi.js

--- plugins/info-donasi.js
+++ plugins/info-donasi.js
@@ -2,10 +2,10 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
 let text = `
 「 Suport Bot Dengan Cara donasi 」
 
-⫹⫺ Via Pulsa Indosat : 6285850539404
-⫹⫺ Via Dana : 6285850539404
+⫹⫺ Via Pulsa axis : 6283813417529
+⫹⫺ Via Dana : 6281352724422
 ⫹⫺ Via Saweria :
-⫹⫺ https://saweria.co/jarotoffc
+⫹⫺ https://saweria.co/khurushi5
 
 `.trim()
   m.reply(text)
